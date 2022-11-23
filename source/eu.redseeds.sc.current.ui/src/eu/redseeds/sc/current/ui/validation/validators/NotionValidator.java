6f9bb0101caef2fa83fff90ba7f05a39a4ccaa2e
Kamil Rybiński <kamilri@wp.pl>
2022-11-23 11:43:56 +0100

    Error fixes for 7.4 #3

--- source/eu.redseeds.sc.current.ui/src/eu/redseeds/sc/current/ui/validation/validators/NotionValidator.java
+++ source/eu.redseeds.sc.current.ui/src/eu/redseeds/sc/current/ui/validation/validators/NotionValidator.java
@@ -43,178 +43,186 @@ public class NotionValidator implements IValidate {
 		NotionDTO not = (NotionDTO) element;
 		List<ValidationResult> list = new ArrayList<ValidationResult>();
 		ValidationResult vr = null;
-		if(!validateNotionDuplicate(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_DUPLICATE);
-			vr.setMessage(MSG_NOTION_DUPLICATE);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_ERROR);
-			list.add(vr);
-		}
-		if(!validateList(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_LIST_WITHOUT_ELEMENTS);
-			vr.setMessage(MSG_NOTION_LIST_WITHOUT_ELEMENTS);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_ERROR);
-			list.add(vr);
-		}
-		if(!validateSimpleView(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_SIMPLE_VIEW_WITHOUT_ELEMENTS);
-			vr.setMessage(MSG_NOTION_SIMPLE_VIEW_WITHOUT_ELEMENTS);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_ERROR);
-			list.add(vr);
-		}
-		if(!validateAttribute(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_ATTRIBUTE_WITHOUT_PARENT);
-			vr.setMessage(MSG_NOTION_ATTRIBUTE_WITHOUT_PARENT);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_WARNING);
-			list.add(vr);
-		}
-		if(!validateAttributeVisibility(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_INVISIBLE_NOTION_ATTRIBUTE);
-			vr.setMessage(MSG_INVISIBLE_NOTION_ATTRIBUTE);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_INFO);
-			list.add(vr);
-		}
-		if(!validateAttributeAttachment(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_UNATTACH_NOTION_ATTRIBUTE);
-			vr.setMessage(MSG_UNATTACH_NOTION_ATTRIBUTE);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_INFO);
-			list.add(vr);
-		}
-		if(!validateGeneralizations(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_MULTIPLE_GENERALIZATION);
-			vr.setMessage(MSG_NOTION_MULTIPLE_GENERALIZATION);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_INFO);
-			list.add(vr);
-		}
-		if(not.getType().isEmpty() && !MNotion.canBeConcept(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_CONCEPT);
-			vr.setMessage(MSG_NOTION_CONCEPT);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_WARNING);
-			list.add(vr);
-		}
-		if(NotionTypesEnum.Screen.tag().equals(not.getType()) && !MNotion.canBeScreen(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_SCREEN);
-			vr.setMessage(MSG_NOTION_SCREEN);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_WARNING);
-			list.add(vr);
-		}
-		if(NotionTypesEnum.Message.tag().equals(not.getType()) && !MNotion.canBeMessage(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_MESSAGE);
-			vr.setMessage(MSG_NOTION_MESSAGE);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_WARNING);
-			list.add(vr);
-		}
-		if(NotionTypesEnum.Confirmation_Dialog.tag().equals(not.getType()) && !MNotion.canBeConfirmationDialog(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_CONFIRMATION_DIALOG);
-			vr.setMessage(MSG_NOTION_CONFIRMATION_DIALOG);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_WARNING);
-			list.add(vr);
-		}
-		if(NotionTypesEnum.Trigger.tag().equals(not.getType()) && !MNotion.canBeTrigger(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_TRIGGER);
-			vr.setMessage(MSG_NOTION_TRIGGER);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_WARNING);
-			list.add(vr);
-		}
-		if(NotionTypesEnum.Attribute.tag().equals(not.getType()) && !MNotion.canBeAttribute(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_ATTRIBUTE);
-			vr.setMessage(MSG_NOTION_ATTRIBUTE);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_WARNING);
-			list.add(vr);
-		}
-		if(NotionTypesEnum.Option.tag().equals(not.getType()) && !MNotion.canBeOption(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_OPTION);
-			vr.setMessage(MSG_NOTION_OPTION);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_WARNING);
-			list.add(vr);
-		}
-		if(NotionTypesEnum.List_View.tag().equals(not.getType()) && !MNotion.canBeListView(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_LIST_VIEW);
-			vr.setMessage(MSG_NOTION_LIST_VIEW);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_WARNING);
-			list.add(vr);
-		}
-		if(NotionTypesEnum.Simple_View.tag().equals(not.getType()) && !MNotion.canBeSimpleView(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_SIMPLE_VIEW);
-			vr.setMessage(MSG_NOTION_SIMPLE_VIEW);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_WARNING);
-			list.add(vr);
-		}
-		if(NotionTypesEnum.Tree_View.tag().equals(not.getType()) && !MNotion.canBeTreeView(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_TREE_VIEW);
-			vr.setMessage(MSG_NOTION_TREE_VIEW);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_WARNING);
-			list.add(vr);
-		}
-		if(Arrays.asList(NotionTypesEnum.viewTags()).contains(not.getType()) && !validateMainConcept(not)){
-			vr = new ValidationResult();
-			vr.setProblemID(IValidate.ID_NOTION_DATA_VIEW_WITHOUT_MAIN_CONCEPT);
-			vr.setMessage(MSG_NOTION_DATA_VIEW_WITHOUT_MAIN_CONCEPT);
-			vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
-			vr.setSclElement(not);
-			vr.setSeverity(IValidate.SEVERITY_WARNING);
-			list.add(vr);
-		}
-		for (NotionDTO par:not.getNotionAttributeParents())
-			if (!MNotion.canExistAddedAtribute(not, par)){
+		try {
+			if(!validateNotionDuplicate(not)){
 				vr = new ValidationResult();
-				vr.setProblemID(IValidate.ID_ATTRIBUTE);
-				vr.setMessage(MSG_ATTRIBUTE);
+				vr.setProblemID(IValidate.ID_NOTION_DUPLICATE);
+				vr.setMessage(MSG_NOTION_DUPLICATE);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_ERROR);
+				list.add(vr);
+			}
+			if(!validateList(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_LIST_WITHOUT_ELEMENTS);
+				vr.setMessage(MSG_NOTION_LIST_WITHOUT_ELEMENTS);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_ERROR);
+				list.add(vr);
+			}
+			if(!validateSimpleView(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_SIMPLE_VIEW_WITHOUT_ELEMENTS);
+				vr.setMessage(MSG_NOTION_SIMPLE_VIEW_WITHOUT_ELEMENTS);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_ERROR);
+				list.add(vr);
+			}
+			if(!validateAttribute(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_ATTRIBUTE_WITHOUT_PARENT);
+				vr.setMessage(MSG_NOTION_ATTRIBUTE_WITHOUT_PARENT);
 				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
 				vr.setSclElement(not);
 				vr.setSeverity(IValidate.SEVERITY_WARNING);
 				list.add(vr);
 			}
+			if(!validateAttributeVisibility(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_INVISIBLE_NOTION_ATTRIBUTE);
+				vr.setMessage(MSG_INVISIBLE_NOTION_ATTRIBUTE);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_INFO);
+				list.add(vr);
+			}
+			if(!validateAttributeAttachment(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_UNATTACH_NOTION_ATTRIBUTE);
+				vr.setMessage(MSG_UNATTACH_NOTION_ATTRIBUTE);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_INFO);
+				list.add(vr);
+			}
+			if(!validateGeneralizations(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_MULTIPLE_GENERALIZATION);
+				vr.setMessage(MSG_NOTION_MULTIPLE_GENERALIZATION);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_INFO);
+				list.add(vr);
+			}
+			if(not.getType().isEmpty() && !MNotion.canBeConcept(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_CONCEPT);
+				vr.setMessage(MSG_NOTION_CONCEPT);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_WARNING);
+				list.add(vr);
+			}
+			if(NotionTypesEnum.Screen.tag().equals(not.getType()) && !MNotion.canBeScreen(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_SCREEN);
+				vr.setMessage(MSG_NOTION_SCREEN);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_WARNING);
+				list.add(vr);
+			}
+			if(NotionTypesEnum.Message.tag().equals(not.getType()) && !MNotion.canBeMessage(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_MESSAGE);
+				vr.setMessage(MSG_NOTION_MESSAGE);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_WARNING);
+				list.add(vr);
+			}
+			if(NotionTypesEnum.Confirmation_Dialog.tag().equals(not.getType()) && !MNotion.canBeConfirmationDialog(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_CONFIRMATION_DIALOG);
+				vr.setMessage(MSG_NOTION_CONFIRMATION_DIALOG);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_WARNING);
+				list.add(vr);
+			}
+			if(NotionTypesEnum.Trigger.tag().equals(not.getType()) && !MNotion.canBeTrigger(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_TRIGGER);
+				vr.setMessage(MSG_NOTION_TRIGGER);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_WARNING);
+				list.add(vr);
+			}
+			if(NotionTypesEnum.Attribute.tag().equals(not.getType()) && !MNotion.canBeAttribute(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_ATTRIBUTE);
+				vr.setMessage(MSG_NOTION_ATTRIBUTE);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_WARNING);
+				list.add(vr);
+			}
+			if(NotionTypesEnum.Option.tag().equals(not.getType()) && !MNotion.canBeOption(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_OPTION);
+				vr.setMessage(MSG_NOTION_OPTION);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_WARNING);
+				list.add(vr);
+			}
+			if(NotionTypesEnum.List_View.tag().equals(not.getType()) && !MNotion.canBeListView(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_LIST_VIEW);
+				vr.setMessage(MSG_NOTION_LIST_VIEW);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_WARNING);
+				list.add(vr);
+			}
+			if(NotionTypesEnum.Simple_View.tag().equals(not.getType()) && !MNotion.canBeSimpleView(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_SIMPLE_VIEW);
+				vr.setMessage(MSG_NOTION_SIMPLE_VIEW);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_WARNING);
+				list.add(vr);
+			}
+			if(NotionTypesEnum.Tree_View.tag().equals(not.getType()) && !MNotion.canBeTreeView(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_TREE_VIEW);
+				vr.setMessage(MSG_NOTION_TREE_VIEW);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_WARNING);
+				list.add(vr);
+			}
+			if(Arrays.asList(NotionTypesEnum.viewTags()).contains(not.getType()) && !validateMainConcept(not)){
+				vr = new ValidationResult();
+				vr.setProblemID(IValidate.ID_NOTION_DATA_VIEW_WITHOUT_MAIN_CONCEPT);
+				vr.setMessage(MSG_NOTION_DATA_VIEW_WITHOUT_MAIN_CONCEPT);
+				vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+				vr.setSclElement(not);
+				vr.setSeverity(IValidate.SEVERITY_WARNING);
+				list.add(vr);
+			}
+			for (NotionDTO par:not.getNotionAttributeParents())
+				if (!MNotion.canExistAddedAtribute(not, par)){
+					vr = new ValidationResult();
+					vr.setProblemID(IValidate.ID_ATTRIBUTE);
+					vr.setMessage(MSG_ATTRIBUTE);
+					vr.setPath(not.getSpecificationPath()+"\\"+not.getName());
+					vr.setSclElement(not);
+					vr.setSeverity(IValidate.SEVERITY_WARNING);
+					list.add(vr);
+				}
+		} catch (Exception e) {
+			vr = new ValidationResult();
+			vr.setProblemID(IValidate.ID_NOTION_EXCEPTION);
+			vr.setMessage(MSG_NOTION_EXCEPTION);
+			vr.setSclElement(element);
+			list.add(vr);
+		}
 		return list.toArray(new ValidationResult[0]);
 	}
 
